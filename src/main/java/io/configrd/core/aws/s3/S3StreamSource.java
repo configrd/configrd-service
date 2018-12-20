6aa9fe096b17ba666dbfd5d331e2d1f9d37ef0d1
kkarski <kkarski@pop-os.localdomain>
2018-12-19 21:28:59 -0800

    imporoved error handling

--- src/main/java/io/configrd/core/aws/s3/S3StreamSource.java
+++ src/main/java/io/configrd/core/aws/s3/S3StreamSource.java
@@ -92,7 +92,8 @@ public class S3StreamSource implements StreamSource, AdHocStreamSource {
     } catch (AmazonS3Exception e) {
 
       if (e.getStatusCode() != 404) {
-        logger.error(e.getMessage(), e);
+        logger.error(e.getMessage());
+        Throwables.propagate(e);
       }
 
     } catch (IOException io) {
