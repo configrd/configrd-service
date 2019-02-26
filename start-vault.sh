ddd38ef8249392bfad05bbf584e0c8d4df09fe90
Krzysztof Karski <kkarski@appcrossings.com>
2019-02-26 14:44:44 -0800

    refactored vault

--- start-vault.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-/opt/hashicorp/vault server -dev &
-export VAULT_ADDR=http://127.0.0.1:8200
-sleep 2
-/opt/hashicorp/vault auth enable userpass
-/opt/hashicorp/vault write auth/userpass/users/test password=password policies=admins
-/opt/hashicorp/vault policy write admins src/test/resources/vault/admin.hcl
