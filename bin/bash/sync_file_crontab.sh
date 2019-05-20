be3fc3f9b8529ef3eb986c15fd54378bd4c5f876
Krzysztof Karski <kkarski@users.noreply.github.com>
2019-05-20 16:02:14 -0700

    added slash

--- bin/bash/sync_file_crontab.sh
+++ bin/bash/sync_file_crontab.sh
@@ -4,7 +4,7 @@ PATH=/usr/local/bin:/usr/local/sbin:~/bin:/usr/bin:/bin:/usr/sbin:/sbin
 
 #
 #   Usage: ./sync_file_crontab.sh <from> <to>
-#   Example: ./sync_file_crontab.sh https://demo.configrd.io/configrd/v1/apps/env/myapp /tmp/myapp_env.properties
+#   Example: ./sync_file_crontab.sh https://demo.configrd.io/configrd/v1/apps/env/myapp/ /tmp/myapp_env.properties
 #
 #   Sync file with repository every hour
 #   Cron Example: * * * 1 * ./sync_file_crontab.sh https://demo.configrd.io/configrd/v1/apps/env/myapp /tmp/myapp_env.properties
