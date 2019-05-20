528163599b66941bf4a7aeefe116fed47d45e2df
Krzysztof <krzysztof.karski@emergenttech.com>
2019-05-20 16:33:35 -0700

    fixed cron

--- bin/bash/sync_file_crontab.sh
+++ bin/bash/sync_file_crontab.sh
@@ -6,7 +6,7 @@ PATH=/usr/local/bin:/usr/local/sbin:~/bin:/usr/bin:/bin:/usr/sbin:/sbin
 #   Usage: ./sync_file_crontab.sh <from> <to>
 #   Example: ./sync_file_crontab.sh https://demo.configrd.io/configrd/v1/apps/env/myapp/ /tmp/myapp_env.properties
 #
-#   Sync file with repository every hour
-#   Cron Example: * 1 * * * ./sync_file_crontab.sh https://demo.configrd.io/configrd/v1/apps/env/myapp /tmp/myapp_env.properties
+#   Sync file with repository in the first minute of every hour
+#   Cron Example: 0 */1 * * * sync_file_crontab.sh https://demo.configrd.io/configrd/v1/apps/env/myapp /tmp/myapp_env.properties
 #   
 curl -s -H "Accept: text/plain" $1 > $2
